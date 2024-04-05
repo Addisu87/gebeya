5150a127a05e8ff75b312d03af4f318543eedcab
Addisu <addisuhaile87@gmail.com>
2024-04-05 17:49:29 +0300

    Remove order-detail.tsx file

--- gebeya-store/src/admin/components/onboarding-flow/nextjs/orders/order-detail.tsx
+++
@@ -1,8 +0,0 @@
-const OrderDetailNextjs = () => {
-	const queryParams = `?ref=onboarding&type=${
-		process.env.MEDUSA_ADMIN_ONBOARDING_TYPE || 'nextjs'
-	}`;
-	return <></>;
-};
-
-export default OrderDetailNextjs;
