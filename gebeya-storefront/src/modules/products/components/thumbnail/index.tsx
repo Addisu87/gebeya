b369ffa911d8fecf76f5b2366e1b9f5d1f34ebab
Addisu <addisuhaile87@gmail.com>
2024-04-11 16:15:41 +0300

    Update home page

--- gebeya-storefront/src/modules/products/components/thumbnail/index.tsx
+++ gebeya-storefront/src/modules/products/components/thumbnail/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import React from "react"
 
 import PlaceholderImage from "@modules/common/icons/placeholder-image"
+import ImageSlider from "../image-slider"
 
 type ThumbnailProps = {
   thumbnail?: string | null
@@ -22,7 +23,8 @@ const Thumbnail: React.FC<ThumbnailProps> = ({
   className,
   "data-testid": dataTestid,
 }) => {
-  const initialImage = thumbnail || images?.[0]?.url
+  const initialImage =
+    thumbnail || (images && images.length > 0 ? images[0].url : null)
 
   return (
     <Container
@@ -41,7 +43,11 @@ const Thumbnail: React.FC<ThumbnailProps> = ({
       )}
       data-testid={dataTestid}
     >
-      <ImageOrPlaceholder image={initialImage} size={size} />
+      {images && images.length > 1 ? (
+        <ImageSlider urls={images} />
+      ) : (
+        <ImageOrPlaceholder image={initialImage} size={size} />
+      )}
     </Container>
   )
 }
@@ -49,22 +55,26 @@ const Thumbnail: React.FC<ThumbnailProps> = ({
 const ImageOrPlaceholder = ({
   image,
   size,
-}: Pick<ThumbnailProps, "size"> & { image?: string }) => {
-  return image ? (
-    <Image
-      src={image}
-      alt="Thumbnail"
-      className="absolute inset-0 object-cover object-center"
-      draggable={false}
-      quality={50}
-      sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
-      fill
-    />
-  ) : (
-    <div className="w-full h-full absolute inset-0 flex items-center justify-center">
-      <PlaceholderImage size={size === "small" ? 16 : 24} />
-    </div>
-  )
+}: Pick<ThumbnailProps, "size"> & { image?: string | null }) => {
+  if (image && typeof image === "string") {
+    return (
+      <Image
+        src={image}
+        alt="Thumbnail"
+        className="absolute inset-0 object-cover object-center"
+        draggable={false}
+        quality={50}
+        sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+        fill
+      />
+    )
+  } else {
+    return (
+      <div className="w-full h-full absolute inset-0 flex items-center justify-center">
+        <PlaceholderImage size={size === "small" ? 16 : 24} />
+      </div>
+    )
+  }
 }
 
 export default Thumbnail
